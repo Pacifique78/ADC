0a50da2bab4453d20031074075554545575caae2
Pacifique78 <tuyipaccy78@gmail.com>
2019-09-06 09:49:00 +0200

    set dynamic port

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import bodyParser from "body-parser"
 import router from './server/routes/Routes';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());// to create post request
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(router);
-const port= 6003;
+const port= process.env.PORT || 6003;
  app.listen(port,console.log(`Listening to port ${port}`));
  
  export default app;
