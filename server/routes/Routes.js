01006fc96f75b5540e47f028f6cc00a4e1ec506c
Pacifique78 <tuyipaccy78@gmail.com>
2019-09-06 13:58:18 +0200

    change .travis.yml file [Fixes #168340791]

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -2,7 +2,6 @@ import newclass from '../controllers/UsersControler';
 import express from 'express';
 
 const router=express.Router(); 
-
 router.post('/api/v1/auth/signup',newclass.createUser);
 router.post('/api/v1/auth/signin',newclass.Login);
 router.patch('/api/v1/user/:userId', newclass.becomeMentor);
