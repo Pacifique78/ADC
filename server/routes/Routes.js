865ec92bce58153f9c8707d0c66d1d8de00bf555
Pacifique78 <tuyipaccy78@gmail.com>
2019-09-11 22:48:41 +0200

    Fix the travis error [Improves #168383702]

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -7,5 +7,4 @@ import checkUser from '../middleware/newUserSchema';
 
 const router=express.Router(); 
 router.post('/api/v2/auth/signup', checkUser, newclass.createUser);
-router.get('/api/users', newclass.findUsers);
 export default router;
